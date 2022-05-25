90d9589166390f9d1377db8dcfa45e383375b319
Naod-A <nabraham@kabbee.org>
2022-05-25 22:00:03 +0300

    only 2 files added

--- flutter_sample/lib/sample.dart
+++ flutter_sample/lib/sample.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sample demo',
+      title: 'Sample demo 123',
       theme: ThemeData(
         // This is the theme of your application.
         //
